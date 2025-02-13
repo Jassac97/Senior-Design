891903e9d3c7421e3763b1ba3f73c5e9088142d2
Jassac97 <jbradfield97@gmail.com>
2025-02-13 15:56:11 -0800

    Add files via upload

--- EE175B_StateMachines.ino
+++ EE175B_StateMachines.ino
@@ -8,17 +8,29 @@ Servo shoulderServo; //shoulder servo
 Servo wristServo; //wrist servo
 Servo clawServo; //claw servo
 
-enum STATE {S1, S2, S3} gState;
+const int buttonPin = 2; //button value 
+const int GreenLEDPin = 3; // green LED pin
+const int RedLEDPin = 4; //Red LED pin
+
+enum STATE {S1, S2, S3} gState = S1;
 //S1 is state where player is making move. Robot is waiting to receive move
 //S2 Chess engine decides move for robot. Sends signal to robot
 //S3 is when robot makes move. It then resets back to original position.
-int B = 0; //button value 
+
+int buttonState = 0; //Button state. 0 = not pressed
 
 void RobotFSM(void) {
+  buttonState = digitalRead(buttonPin);
 
   //transitions
   switch(gState) {
     case S1: 
+    if (buttonState == HIGH) {
+      gState = S2;
+    }
+    else{
+      gState = S1;
+    }
 
     break;
 
@@ -39,6 +51,8 @@ void RobotFSM(void) {
   switch(gState) {
 
     case S1:
+    digitalWrite(GreenLEDPin, HIGH); //Green LED On
+    digitalWrite(RedLEDPin, LOW); //Red LED Off
     //player makes move
     //robot waits for move in initial position
     //player hits button after move is complete to send signal to chess engine
@@ -46,12 +60,16 @@ void RobotFSM(void) {
     break;
 
     case S2:
+    digitalWrite(GreenLEDPin, LOW); //Green LED off
+    digitalWrite(RedLEDPin, LOW); //Red LED Off
     //chess engine recieves player's move data
     //engine decides what move the robot should make and sends the information to robot
 
     break;
 
     case S3:
+    digitalWrite(GreenLEDPin, LOW); //Green LED Off
+    digitalWrite(RedLEDPin, HIGH); //Red LED On
     //robot recieves data from chess engine on move it needs to make
     //robot proceeds to make move
     //once robot makes move, it resets back in original position
@@ -70,12 +88,13 @@ void setup() {
   shoulderServo.attach(10);
   wristServo.attach(11);
   clawServo.attach(12);
-  pinMode(2, INPUT); //button
+  pinMode(buttonPin, INPUT); //button
+  pinMode(GreenLEDPin, OUTPUT); //Green LED
+  pinMode(RedLEDPin, OUTPUT); //Red LED
 
 }
 
 void loop() {
-  B = digitalRead(2);
   RobotFSM();
   delay(100);
 }
